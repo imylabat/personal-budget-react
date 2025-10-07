d26095f2ccd11d355d370bd325636cad396b941d
imylabat <imylabat@gmail.com>
2025-10-07 09:46:43 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+        <h2>WELCOME!</h2>
+        <h2>To your personal-budget management app</h2>
+    </div>
+  );
+}
+
+export default Hero;
